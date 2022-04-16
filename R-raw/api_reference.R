01e21e9cd16339ba108b63056f0fe0434cf8395a
Jon Harmon <jonthegeek@gmail.com>
2022-04-16 16:21:20 -0500

    Start figuring out swagger import.

--- R-raw/api_reference.R
+++ R-raw/api_reference.R
@@ -1,18 +1,46 @@
-# Scrape initial versions of functions from the API reference.
+## Adapted from
+## https://github.com/rstudio/swagger/issues/1#issuecomment-395627756 by
+## hrbrmstr
 
-library(magrittr)
+library(glue)
+library(styler)
+library(tidyverse)
+library(jsonlite)
 
-page <- xml2::read_html("https://marketplace.zoom.us/docs/api-reference/zoom-api/accounts/accounts")
+# OpenAPI spec sourced from
+# https://marketplace.zoom.us/docs/api-reference/zoom-api/methods
+api_spec <- jsonlite::read_json("R-raw/swagger.json")
+base_path <- api_spec$servers[[1]]$url
 
-page
+methods <- tibble::enframe(
+  x = api_spec$paths,
+  name = "path"
+) %>%
+  tidyr::unnest_wider(value) %>%
+  tidyr::pivot_longer(
+    get:put,
+    names_to = "http"
+  ) %>%
+  dplyr::mutate(
+    http = toupper(http),
+    is_null = purrr::map_lgl(value, is.null)
+  ) %>%
+  dplyr::filter(!is_null) %>%
+  dplyr::select(-is_null) %>%
+  tidyr::unnest_wider(
+    value
+  ) %>%
+  dplyr::filter(is.na(deprecated) | !deprecated) %>%
+  dplyr::select(-deprecated)
 
-method <- rvest::html_nodes(page, ".TextBlock-header-token") %>%
-  rvest::html_text()
+methods
 
-rvest::html_nodes(page, ".HubBlockList")[[1]] %>%
-  rvest::html_nodes(".mr-3") %>%
-  rvest::html_text()
+# I *think* I want to divide it into a file for each path start, and then the
+# rest of the path (minus anything in {}) is the function name. The {} ones are
+# a special case, and those things should become parameters to the function. I
+# shouldn't call that a special case, though, because that's 201 of 253 methods.
+# This plan isn't exactly right yet. See things that start with
+# "/metrics/meetings" for some definite counter-examples.
 
-fun_args <- page %>%
-  rvest::html_table() %>%
-  purrr::pluck(2)
+# "/metrics/meetings/{meetingId}/participants/{participantId}/qos" feels like it
+# should be something like meeting_participant_qos.
